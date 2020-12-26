3b47f1ee101625cf4cd4a84f84e1949855d4492e
Jakub Vala <jamm3e3333@gmail.com>
2020-12-26 10:36:24 +0100

    dataset options

--- 1/app.js
+++ 1/app.js
@@ -5,15 +5,21 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
-
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August','September','October','December'],
+        datasets: [
+        {
+            label: 'My profits',
+            backgroundColor: 'rgba(0, 212, 183,0.1)',
+            borderColor: 'blue',
+            borderWidth: 2.5,
+            borderDash: [10],
+            borderDashOffset: 10,
+            borderCapStyle: 'square',
+            borderJoinStyle: 'bevel',
+            cubicInterpolationMode: 'default',
+            data: [-2, 3, 7, 2, 0, -10, 8,13,2,8,-3,6,8]
+        }
+    ]},
     // Configuration options go here
     options: {}
 });
