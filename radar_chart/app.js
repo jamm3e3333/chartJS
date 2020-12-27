3b22fbedfd46e4109a31ac3260c641ab02ad768c
Jakub Vala <jamm3e3333@gmail.com>
2020-12-27 22:54:58 +0100

    color changing

--- radar_chart/app.js
+++ radar_chart/app.js
@@ -5,8 +5,8 @@ var chart = new Chart(ctx,{
         labels: ['Drinking','Eating','Sleeping','Working','Studying'],
         datasets: [{
             lable: 'Radar chart',
-            backgroundColor: 'rgba(0, 141, 207,0.7)',
-            borderColor: 'blue',
+            backgroundColor: 'rgba(255, 87, 101,0.7)',
+            borderColor: 'red',
             borderWidth: 3,
             data: [80,32,68,15,90]
         }]
