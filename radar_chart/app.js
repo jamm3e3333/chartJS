89a09590c3fe51d4ff5acbacee349414a3b8f63c
Jakub Vala <jamm3e3333@gmail.com>
2020-12-28 12:38:06 +0100

    finishing the radar chart

--- radar_chart/app.js
+++ radar_chart/app.js
@@ -7,7 +7,22 @@ var chart = new Chart(ctx,{
             lable: 'Radar chart',
             backgroundColor: 'rgba(255, 87, 101,0.7)',
             borderColor: 'red',
-            borderWidth: 3,
+            borderWidth: 8,
+            //borderDash: [5,15,2,15],
+            borderDashOffset: 0,
+            borderCapStyle: 'square',
+            borderJoinStyle: 'round',
+            // lineTension: 0.2, the bigger the more rounded the line is, default is 0 (straight)
+            pointBackgroundColor: 'rgb(46, 191, 155)',
+            pointBorderColor: 'pink',
+            pointBorderWidth: 2,
+            pointRadius: 8,
+            pointStyle: 'rectRot',
+            pointHitRadius: 8,
+            pointHoverBackgroundColor: 'rgba(72, 86, 247,0.6)',
+            pointHoverBorderColor: 'rgb(145, 72, 247)',
+            pointHoverBorderWidth: 3,
+            pointHoverRadius: 15,
             data: [80,32,68,15,90]
         }]
     },
