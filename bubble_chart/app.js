d0f2f920d4b033e53bc5be5cfbbece43511fa78e
Jakub Vala <jamm3e3333@gmail.com>
2020-12-29 14:09:02 +0100

    bubble chart done

--- bubble_chart/app.js
+++ bubble_chart/app.js
@@ -2,9 +2,18 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx,{
     type: 'bubble',
     data:{
-        labels: ['January','February','March','April','May'],
         datasets: [{
             label: 'bubble chart',
+            backgroundColor: ['rgba(164, 66, 245,0.4)','rgba(66, 138, 245,0.4)','rgba(66, 245, 182,0.4)',
+                                'rgba(209, 245, 66,0.4)','rgba(245, 72, 66,0.4)'],
+            borderColor: 'silver',
+            borderWidth: 2.5,
+            hoverBackgroundColor: 'rgba(245, 66, 66,0.2)',
+            hoverBorderColor: 'red',
+            hoverBorderWidth: 3,
+            hoverRadius: 5,
+            hitRadius: 10,
+            pointStyle: 'rectRot',
             data: [
                 {
                     x:0,y:5,r:14
