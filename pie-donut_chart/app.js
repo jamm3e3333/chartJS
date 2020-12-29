0a5ba38be951d466ca334b6027947310008caa1b
Jakub Vala <jamm3e3333@gmail.com>
2020-12-29 12:43:10 +0100

    finished gauge chart

--- pie-donut_chart/app.js
+++ pie-donut_chart/app.js
@@ -9,38 +9,16 @@ var chart = new Chart(ctx,{
             borderAlign: 'innner',
             borderColor: 'white',
             borderWidth: 4,
-            hoverBackgroundColor: 'rgba(97, 255, 121,0.4)',
-            hoverBorderColor: 'rgba(97, 255, 194)',
+            hoverBackgroundColor: 'black',
+            hoverBorderColor: 'white',
             hoverBorderWidth: 4,
             data: [20,20,15,45]
-        },
-        {
-            label: 'Donut Chart',
-            backgroundColor: ['pink','yellow','#61c8ff','lightblue'],
-            borderAlign: 'innner',
-            borderColor: 'white',
-            borderWidth: 4,
-            hoverBackgroundColor: 'rgba(97, 255, 121,0.4)',
-            hoverBorderColor: 'rgba(97, 255, 194)',
-            hoverBorderWidth: 4,
-            data: [9,30,89,39]
-        },
-        {
-            label: 'Donut Chart',
-            backgroundColor: ['pink','yellow','#61c8ff','lightblue'],
-            borderAlign: 'innner',
-            borderColor: 'white',
-            borderWidth: 4,
-            hoverBackgroundColor: 'rgba(97, 255, 121,0.4)',
-            hoverBorderColor: 'rgba(97, 255, 194)',
-            hoverBorderWidth: 4,
-            data: [19,23,45,94]
         }]
     },
     options: {
-        cutoutPercentage: 30,
-        rotation: -0.25*Math.PI,
-        circumference: 2*Math.PI,
+        cutoutPercentage: 60,
+        rotation: 0.8*Math.PI,
+        circumference: 1.4*Math.PI,
         animation: {
             animateRotate: true,
             animateScale: true
