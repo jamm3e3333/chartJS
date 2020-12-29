7de8d32fddb7de8aa03430a41a3844cfb3cb8ff6
Jakub Vala <jamm3e3333@gmail.com>
2020-12-29 13:26:06 +0100

    polar chart set up

---
+++ polar_chart/app.js
@@ -0,0 +1,13 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx,{
+    type: 'horizontalBar',
+    data:{
+        labels: [''],
+        datasets: [{
+
+        }]
+    },
+    options: {
+        
+    }
+})
