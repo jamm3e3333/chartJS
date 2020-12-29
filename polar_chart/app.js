bbc8128abef44d9124fb10008013349179bde1ec
Jakub Vala <jamm3e3333@gmail.com>
2020-12-29 13:43:51 +0100

    polarArea-chart

--- polar_chart/app.js
+++ polar_chart/app.js
@@ -1,13 +1,24 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx,{
-    type: 'horizontalBar',
+    type: 'polarArea',
     data:{
-        labels: [''],
+        labels: ['January','February','March','April','May'],
         datasets: [{
-
+            backgroundColor: ['rgba(66, 135, 245,0.4)','rgba(66, 245, 197,0.4)','rgba(170, 245, 66,0.4)',
+                                'rgba(245, 239, 66,0.4)','rgba(245, 72, 66,0.4)'],
+            borderColor: 'white',
+            borderWidth: 3,
+            hoverBackgroundColor: 'grey',
+            hoverBorderColor: 'white',
+            hoverBorderWidth: 1,
+            data: [23,90,62,32,15]
         }]
     },
     options: {
-        
+        startAngle: -0.5*Math.PI,
+        animation: {
+            animateRotate: true,
+            animateScale: false
+        }
     }
 })
